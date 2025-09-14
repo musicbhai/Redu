e8a3f702503876ac61e9728bb5c3b5e3f143fac5
musicbhai <dghffyhcyucfuhvfuhc@gmail.com>
2025-09-14 12:17:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/linkers697/Redhuq",
+    "https://github.com/musicbhai/Redu",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
